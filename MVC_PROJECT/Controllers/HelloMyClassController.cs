00acd83f04ee48ca82efb8febd0133194bf039b2
lanvadieppanda1 <logn145236@gmail.com>
2021-04-28 10:46:36 +0700

    commit lần đầu

---
+++ MVC_PROJECT/Controllers/HelloMyClassController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_PROJECT.Controllers
+{
+    public class HelloMyClassController : Controller
+    {
+        // GET: HelloMyClass
+        public ActionResult Index()
+        {
+            return View();
+        }h
+
+    }
+}
---
+++ MVC_PROJECT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_PROJECT.MvcApplication" Language="C#" %>
